d92168b5d81d59ea7097db1016d61c13a3d2599b
DESKTOP-2Q8H8VU\User <deysonurrego159@gmail.com>
2025-10-24 18:57:38 -0500

    Proyecto

--- test/test_calculadora.py
+++ test/test_calculadora.py
@@ -1,4 +1,7 @@
 import unittest
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from app.calculadora import sumar, restar, multiplicar, dividir
 
 class TestCalculadora(unittest.TestCase):
